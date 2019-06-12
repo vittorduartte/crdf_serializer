f381ffd8e6965cb7c72a3d07da59e932a3ce34a0
Mateus Vitor <mateusriograndense@gmail.com>
2019-06-12 19:22:44 -0300

    Criação dos arquivos principais para o funcionamento e distribuição do pacóte.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Custom RDF Serializer Package - CRDF
+
+## 1. Requirements 
+This module requires the rdflib package installation:
+> $ pip install rdflib
+
+## 2. How to Install
+For install this package execute the command in your environment:
+> $ pip install crdf_serializer
+
+
+## 3. How to use
+
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open ( "README.md" , "r" ) as fh :
+    long_description = fh . read ()
+
+setuptools . setup (
+    name = "crdf-serializer" ,
+    version = "0.0.1" ,
+    author = "Prof. Sergio Costa" ,
+    author_email = "prof.sergio.costa@gmail.com" ,
+    description = "Some decorators responsible for preparing and serializing RDF files." ,
+    long_description = long_description ,
+    long_description_content_type = "text/markdown" ,
+    url = "https://github.com/vittorduartte/personal_rdf_package" ,
+    packages = setuptools . find_packages (),
+    classifiers = [
+        "Programming Language :: Python :: 3" ,
+        "License :: OSI Approved :: MIT License" ,
+        "Operating System :: OS Independent" ,
+        "Open Data :: Open Linked Data"
+    ],
+)
